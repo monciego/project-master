348813715cb2329dd5765975e99d1b0017fdd0d1
monciego <jerichobantiquete@gmail.com>
2024-03-30 05:07:24 +0800

    feat: add navbar component

--- resources/js/Layouts/AuthenticatedLayout.tsx
+++ resources/js/Layouts/AuthenticatedLayout.tsx
@@ -5,6 +5,7 @@ import NavLink from "@/Components/NavLink";
 import ResponsiveNavLink from "@/Components/ResponsiveNavLink";
 import { Link } from "@inertiajs/react";
 import { User } from "@/types";
+import Navbar from "@/Components/navbar";
 
 export default function Authenticated({
   user,
@@ -16,6 +17,7 @@ export default function Authenticated({
 
   return (
     <div className="min-h-screen bg-gray-100 dark:bg-slate-950">
+      <Navbar />
       <nav className="bg-white border-b border-gray-100">
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
           <div className="flex justify-between h-16">
