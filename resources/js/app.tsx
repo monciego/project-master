84c1d4478b5829a6f789e71869dd428f856dc0dd
monciego <jerichobantiquete@gmail.com>
2024-03-30 04:33:42 +0800

    feat: install shadcn and add dark mode

--- resources/js/app.tsx
+++ resources/js/app.tsx
@@ -4,23 +4,28 @@ import "../css/app.css";
 import { createRoot } from "react-dom/client";
 import { createInertiaApp } from "@inertiajs/react";
 import { resolvePageComponent } from "laravel-vite-plugin/inertia-helpers";
+import { ThemeProvider } from "./Components/theme-provider";
 
 const appName = import.meta.env.VITE_APP_NAME || "Task Master";
 
 createInertiaApp({
-    title: (title) => `${title} - ${appName}`,
-    resolve: (name) =>
-        resolvePageComponent(
-            `./Pages/${name}.tsx`,
-            import.meta.glob("./Pages/**/*.tsx")
-        ),
-    setup({ el, App, props }) {
-        const root = createRoot(el);
+  title: (title) => `${title} - ${appName}`,
+  resolve: (name) =>
+    resolvePageComponent(
+      `./Pages/${name}.tsx`,
+      import.meta.glob("./Pages/**/*.tsx")
+    ),
+  setup({ el, App, props }) {
+    const root = createRoot(el);
 
-        root.render(<App {...props} />);
-    },
-    progress: {
-        color: "#0007d7",
-        showSpinner: true,
-    },
+    root.render(
+      <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+        <App {...props} />
+      </ThemeProvider>
+    );
+  },
+  progress: {
+    color: "#0007d7",
+    showSpinner: true,
+  },
 });
